85f9e6f17a01521e1f67dcca15b356077b8dbb5d
Maria Clara <maria.clara112811@gmail.com>
2025-04-07 15:50:22 -0300

    Alt.

--- git.js
+++ git.js
@@ -3,4 +3,4 @@ console.log (`a lista de todas as comidas ${comidas}`);
 console.log (`primeira comida:${comidas[0]}`);
 comidas.SHOR();
 console.log (`comidas ordenadas:${comidas}`)
-console,log('FIM')
+console.log('FIM')
