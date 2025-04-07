1776bc14fee5314caaff68fa93cec106f79ace98
Maria Clara <maria.clara112811@gmail.com>
2025-04-07 15:18:50 -0300

    incluíndo ordenação da lista

--- git.js
+++ git.js
@@ -1,3 +1,5 @@
 let comidas = [`lasanha`,`pizza`, `Sushi`, `hamburguer`]; 
 console.log (`a lista de todas as comidas ${comidas}`); 
-console.log (`primeira comida:${comidas[0]}`)
+console.log (`primeira comida:${comidas[0]}`);
+comidas.SHOR();
+console.log (`comidas ordenadas:${comidas}`)
